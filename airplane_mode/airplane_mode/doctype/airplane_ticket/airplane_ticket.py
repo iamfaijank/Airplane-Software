a588606f64cac691c8909783ffdf48cfe1cef37f
iamfaijank <iamfaijankq@gmail.com>
2025-07-03 15:04:06 +0530

    feat:Ticket amount and add-on is calculate completely

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2025, faizan and contributors
 # For license information, please see license.txt
+# airplane_ticket.py
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class AirplaneTicket(Document):
-	pass
+	def validate(self):
+		self.remove_duplicate_add_ons()
+		self.calculate_total_amount()
+
+	def remove_duplicate_add_ons(self):
+		seen = set()
+		unique_add_ons = []
+		for row in self.add_ons:
+			if row.item not in seen:
+				seen.add(row.item)
+				unique_add_ons.append(row)
+		self.set("add_ons", unique_add_ons)
+
+	def calculate_total_amount(self):
+		total_addon_amount = sum(addon.item_amount for addon in self.add_ons)
+		self.total_amount = (self.flight_price or 0) + total_addon_amount
+
+	def before_submit(self):
+		if self.status != "Boarded":
+			frappe.throw("Only tickets with status 'Boarded' can be submitted.")
